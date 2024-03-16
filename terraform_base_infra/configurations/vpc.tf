1c8b6fb536763cb008e6db0a8f1faca834ab382a
najeebkhot <najeebkhot.1@gmail.com>
2024-03-16 22:59:06 +0530

    commented vpc file

--- terraform_base_infra/configurations/vpc.tf
+++ terraform_base_infra/configurations/vpc.tf
@@ -1,11 +1,11 @@
 #This file will be used as a module file for vpc creation
 
-module "vpc" {
-    source = "git@github.com:najeebkhot/terraform-modules?ref=main"
+# module "vpc" {
+#     source = "git@github.com:najeebkhot/terraform-modules?ref=main"
 
-    cidr_block = var.cidr_block
-    instance_tenancy = var.instance_tenancy
-    enable_dns_support = var.enable_dns_support
-    enable_dns_hostnames = var.enable_dns_hostnames
-    vpc_tags = var.vpc_tags
-}
+#     cidr_block = var.cidr_block
+#     instance_tenancy = var.instance_tenancy
+#     enable_dns_support = var.enable_dns_support
+#     enable_dns_hostnames = var.enable_dns_hostnames
+#     vpc_tags = var.vpc_tags
+# }
