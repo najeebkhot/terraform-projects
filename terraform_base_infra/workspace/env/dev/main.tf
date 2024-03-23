cf7f23a252fc42df785a0caf7aacb9623c4cc439
najeebkhot <najeebkhot.1@gmail.com>
2024-03-23 12:23:34 +0530

    Added another environment folder

---
+++ terraform_base_infra/workspace/env/dev/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region      = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+    # skip_metadata_api_check = true
+    # skip_region_validation = true
+    # skip_credentials_validation = true
+    # skip_requesting_account_id = true
+}
+
+# terraform {
+#   backend "s3" {
+#     bucket          = ""
+#     key             = ""
+#     dynamodb_table  = ""
+#     encrypt         = true
+#     region          = ""
+#   }
+# }
+
+module "infra" {
+    source = "../../../../terraform_base_infra/configurations"
+
+    region = "us-east-1"
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    vpc_tags = {
+        Name = "Main VPC"
+    }
+}
