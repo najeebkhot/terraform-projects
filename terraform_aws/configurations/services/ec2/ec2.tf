6d77bb536ef37b5d706936687e68f5bf16562dc3
najeebkhot <najeebkhot.1@gmail.com>
2024-04-09 06:53:53 +0530

    Added files for EC2, NIC and Security Group

--- terraform_aws/configurations/services/ec2/ec2.tf
+++ terraform_aws/configurations/services/ec2/ec2.tf
@@ -1,12 +1,6 @@
 resource "aws_instance" "ec2" {
   ami           = var.ec2_ami
-  instance_type = "t2.micro"
-  network_interface {
-    network_interface_id = aws_network_interface.nic.id
-    device_index         = 0
-  }
-
-  tags = {
-    Name = "webserver01"
-  }
+  instance_type = var.ec2_instance_type
+  subnet_id     = var.subnet_id
+  tags          = var.ec2_name_tag
 }
--- terraform_aws/configurations/services/nic/nic.tf
+++ terraform_aws/configurations/services/nic/nic.tf
@@ -1,4 +1,4 @@
 resource "aws_network_interface" "nic" {
-  subnet_id   = var.aws_public_subnet_id
+  subnet_id   = var.subnet_id
   tags = var.nic_name_tag
 }
